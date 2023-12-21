a13cfb6abd89566b77259ce115d556ebc0549a6e
reubert22 <reubertfernandes@gmail.com>
2023-12-21 16:44:24 -0300

    fix: fix route

--- src/data/api/client.ts
+++ src/data/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://api.weatherapi.com/v1/",
+  baseURL: "https://api.weatherapi.com/v1/",
   headers: { Accept: "application/json" },
 });
